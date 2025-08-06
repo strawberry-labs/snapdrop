9a00d700e37e972a14d9187349b567b7a4d31ff1
Chirag Asarpota <70430557+chiragasarpota@users.noreply.github.com>
2025-08-06 10:26:20 +0400

    feat: changed to uWebSocket.js

--- server/index.js
+++ server/index.js
@@ -1,292 +1,190 @@
-var process = require('process')
-// Handle SIGINT
-process.on('SIGINT', () => {
-  console.info("SIGINT Received, exiting...")
-  process.exit(0)
-})
-
-// Handle SIGTERM
-process.on('SIGTERM', () => {
-  console.info("SIGTERM Received, exiting...")
-  process.exit(0)
-})
-
-const parser = require('ua-parser-js');
+const process = require('process');
+const uWS = require('uWebSockets.js');
 const { uniqueNamesGenerator, animals, colors } = require('unique-names-generator');
+const parser = require('ua-parser-js');
 
-class SnapdropServer {
+// Helper to generate a UUID
+const generateUuid = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+        const r = (Math.random() * 16) | 0;
+        const v = c === 'x' ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+    });
+};
 
-    constructor(port) {
-        const WebSocket = require('ws');
-        this._wss = new WebSocket.Server({ port: port });
-        this._wss.on('connection', (socket, request) => this._onConnection(new Peer(socket, request)));
-        this._wss.on('headers', (headers, response) => this._onHeaders(headers, response));
+const port = process.env.PORT || 3000;
 
-        this._rooms = {};
+const app = uWS.App().ws('/*', {
+    /* Options */
+    idleTimeout: 32, // More efficient than manual ping/pong
+    maxBackpressure: 1024,
+    maxPayloadLength: 512,
 
-        console.log('Snapdrop is running on port', port);
-    }
+    /* This handler is called before upgrading the connection to a WebSocket */
+    upgrade: (res, req, context) => {
+        const ua = req.getHeader('user-agent');
+        const ip = Buffer.from(res.getRemoteAddressAsText()).toString();
+        
+        // Handle x-forwarded-for behind a reverse proxy
+        const forwardedFor = req.getHeader('x-forwarded-for');
+        const realIp = forwardedFor ? forwardedFor.split(',')[0].trim() : ip;
 
-    _onConnection(peer) {
-        this._joinRoom(peer);
-        peer.socket.on('message', message => this._onMessage(peer, message));
-        peer.socket.on('error', console.error);
-        this._keepAlive(peer);
-
-        // send displayName
-        this._send(peer, {
-            type: 'display-name',
-            message: {
-                displayName: peer.name.displayName,
-                deviceName: peer.name.deviceName
-            }
-        });
-    }
-
-    _onHeaders(headers, response) {
-        if (response.headers.cookie && response.headers.cookie.indexOf('peerid=') > -1) return;
-        response.peerId = Peer.uuid();
-        headers.push('Set-Cookie: peerid=' + response.peerId + "; SameSite=Strict; Secure");
-    }
-
-    _onMessage(sender, message) {
-        // Try to parse message 
-        try {
-            message = JSON.parse(message);
-        } catch (e) {
-            return; // TODO: handle malformed JSON
-        }
-
-        switch (message.type) {
-            case 'disconnect':
-                this._leaveRoom(sender);
-                break;
-            case 'pong':
-                sender.lastBeat = Date.now();
-                break;
-        }
-
-        // relay message to recipient
-        if (message.to && this._rooms[sender.ip]) {
-            const recipientId = message.to; // TODO: sanitize
-            const recipient = this._rooms[sender.ip][recipientId];
-            delete message.to;
-            // add sender id
-            message.sender = sender.id;
-            this._send(recipient, message);
-            return;
-        }
-    }
-
-    _joinRoom(peer) {
-        // if room doesn't exist, create it
-        if (!this._rooms[peer.ip]) {
-            this._rooms[peer.ip] = {};
-        }
-
-        // notify all other peers
-        for (const otherPeerId in this._rooms[peer.ip]) {
-            const otherPeer = this._rooms[peer.ip][otherPeerId];
-            this._send(otherPeer, {
-                type: 'peer-joined',
-                peer: peer.getInfo()
-            });
-        }
-
-        // notify peer about the other peers
-        const otherPeers = [];
-        for (const otherPeerId in this._rooms[peer.ip]) {
-            otherPeers.push(this._rooms[peer.ip][otherPeerId].getInfo());
-        }
-
-        this._send(peer, {
-            type: 'peers',
-            peers: otherPeers
-        });
-
-        // add peer to room
-        this._rooms[peer.ip][peer.id] = peer;
-    }
-
-    _leaveRoom(peer) {
-        if (!this._rooms[peer.ip] || !this._rooms[peer.ip][peer.id]) return;
-        this._cancelKeepAlive(this._rooms[peer.ip][peer.id]);
-
-        // delete the peer
-        delete this._rooms[peer.ip][peer.id];
-
-        peer.socket.terminate();
-        //if room is empty, delete the room
-        if (!Object.keys(this._rooms[peer.ip]).length) {
-            delete this._rooms[peer.ip];
-        } else {
-            // notify all other peers
-            for (const otherPeerId in this._rooms[peer.ip]) {
-                const otherPeer = this._rooms[peer.ip][otherPeerId];
-                this._send(otherPeer, { type: 'peer-left', peerId: peer.id });
+        // --- Start of Cookie/Peer ID Logic ---
+        let peerId;
+        const cookie = req.getHeader('cookie');
+        if (cookie) {
+            const match = cookie.match(/peerid=([a-f0-9-]+)/);
+            if (match) {
+                peerId = match[1];
             }
         }
-    }
 
-    _send(peer, message) {
-        if (!peer) return;
-        if (this._wss.readyState !== this._wss.OPEN) return;
-        message = JSON.stringify(message);
-        peer.socket.send(message, error => '');
-    }
-
-    _keepAlive(peer) {
-        this._cancelKeepAlive(peer);
-        var timeout = 30000;
-        if (!peer.lastBeat) {
-            peer.lastBeat = Date.now();
+        if (!peerId) {
+            peerId = generateUuid();
+            // Note: uWS sends headers on upgrade, so we can set the cookie here.
+            res.writeHeader('Set-Cookie', `peerid=${peerId}; SameSite=Strict; Secure`);
         }
-        if (Date.now() - peer.lastBeat > 2 * timeout) {
-            this._leaveRoom(peer);
-            return;
-        }
+        // --- End of Cookie/Peer ID Logic ---
 
-        this._send(peer, { type: 'ping' });
+        /* Pass peer data to the 'open' handler */
+        res.upgrade({
+                peerId: peerId,
+                realIp: realIp,
+                userAgent: ua
+            },
+            /* Upgrade headers */
+            req.getHeader('sec-websocket-key'),
+            req.getHeader('sec-websocket-protocol'),
+            req.getHeader('sec-websocket-extensions'),
+            context
+        );
+    },
 
-        peer.timerId = setTimeout(() => this._keepAlive(peer), timeout);
-    }
+    open: (ws) => {
+        // Retrieve data from the upgrade handler
+        ws.id = ws.peerId;
+        ws.ip = ws.realIp;
 
-    _cancelKeepAlive(peer) {
-        if (peer && peer.timerId) {
-            clearTimeout(peer.timerId);
-        }
-    }
-}
-
-
-
-class Peer {
-
-    constructor(socket, request) {
-        // set socket
-        this.socket = socket;
-
-
-        // set remote ip
-        this._setIP(request);
-
-        // set peer id
-        this._setPeerId(request)
-        // is WebRTC supported ?
-        this.rtcSupported = request.url.indexOf('webrtc') > -1;
-        // set name 
-        this._setName(request);
-        // for keepalive
-        this.timerId = 0;
-        this.lastBeat = Date.now();
-    }
-
-    _setIP(request) {
-        if (request.headers['x-forwarded-for']) {
-            this.ip = request.headers['x-forwarded-for'].split(/\s*,\s*/)[0];
-        } else {
-            this.ip = request.connection.remoteAddress;
-        }
-        // IPv4 and IPv6 use different values to refer to localhost
-        if (this.ip == '::1' || this.ip == '::ffff:127.0.0.1') {
-            this.ip = '127.0.0.1';
-        }
-    }
-
-    _setPeerId(request) {
-        if (request.peerId) {
-            this.id = request.peerId;
-        } else {
-            this.id = request.headers.cookie.replace('peerid=', '');
-        }
-    }
-
-    toString() {
-        return `<Peer id=${this.id} ip=${this.ip} rtcSupported=${this.rtcSupported}>`
-    }
-
-    _setName(req) {
-        let ua = parser(req.headers['user-agent']);
-
-
-        let deviceName = '';
-        
-        if (ua.os && ua.os.name) {
-            deviceName = ua.os.name.replace('Mac OS', 'Mac') + ' ';
-        }
-        
-        if (ua.device.model) {
-            deviceName += ua.device.model;
-        } else {
-            deviceName += ua.browser.name;
+        // In-memory room management
+        const rooms = ws.getRooms();
+        if (!rooms[ws.ip]) {
+            rooms[ws.ip] = new Map();
         }
 
-        if(!deviceName)
-            deviceName = 'Unknown Device';
-
+        // Parse user agent for device info
+        const ua = parser(ws.userAgent);
+        const deviceName = `${ua.os.name || ''} ${ua.browser.name || ''}`.trim() || 'Unknown Device';
         const displayName = uniqueNamesGenerator({
-            length: 2,
-            separator: ' ',
             dictionaries: [colors, animals],
             style: 'capital',
-            seed: this.id.hashCode()
-        })
+            separator: ' ',
+            seed: ws.id // Use persistent ID as seed
+        });
 
-        this.name = {
+        ws.name = {
+            deviceName,
+            displayName,
             model: ua.device.model,
             os: ua.os.name,
             browser: ua.browser.name,
             type: ua.device.type,
-            deviceName,
-            displayName
         };
-    }
+        
+        // Subscribe to the room (topic) based on IP
+        ws.subscribe(ws.ip);
 
-    getInfo() {
-        return {
-            id: this.id,
-            name: this.name,
-            rtcSupported: this.rtcSupported
-        }
-    }
+        // Get info of other peers in the room
+        const otherPeers = Array.from(rooms[ws.ip].values()).map(peer => peer.getInfo());
 
-    // return uuid of form xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx
-    static uuid() {
-        let uuid = '',
-            ii;
-        for (ii = 0; ii < 32; ii += 1) {
-            switch (ii) {
-                case 8:
-                case 20:
-                    uuid += '-';
-                    uuid += (Math.random() * 16 | 0).toString(16);
-                    break;
-                case 12:
-                    uuid += '-';
-                    uuid += '4';
-                    break;
-                case 16:
-                    uuid += '-';
-                    uuid += (Math.random() * 4 | 8).toString(16);
-                    break;
-                default:
-                    uuid += (Math.random() * 16 | 0).toString(16);
+        // Send the list of existing peers to the new peer
+        ws.send(JSON.stringify({
+            type: 'peers',
+            peers: otherPeers,
+        }));
+
+        // Add the new peer to the room
+        rooms[ws.ip].set(ws.id, ws);
+        
+        // Announce the new peer to everyone else in the room
+        ws.publish(ws.ip, JSON.stringify({
+            type: 'peer-joined',
+            peer: ws.getInfo(),
+        }));
+        
+        // Send the peer its own display name info
+        ws.send(JSON.stringify({
+            type: 'display-name',
+            message: {
+                displayName: ws.name.displayName,
+                deviceName: ws.name.deviceName
+            }
+        }));
+    },
+
+    message: (ws, message, isBinary) => {
+        const msg = JSON.parse(Buffer.from(message).toString());
+
+        if (msg.to) {
+            // Find the recipient and send the message directly
+            const room = ws.getRooms()[ws.ip];
+            const recipient = room?.get(msg.to);
+            if (recipient) {
+                msg.sender = ws.id;
+                delete msg.to;
+                // Use send, not publish, for direct peer-to-peer messages
+                recipient.send(JSON.stringify(msg), isBinary);
             }
         }
-        return uuid;
-    };
-}
+    },
 
-Object.defineProperty(String.prototype, 'hashCode', {
-  value: function() {
-    var hash = 0, i, chr;
-    for (i = 0; i < this.length; i++) {
-      chr   = this.charCodeAt(i);
-      hash  = ((hash << 5) - hash) + chr;
-      hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-  }
+    close: (ws, code, message) => {
+        const rooms = ws.getRooms();
+        const room = rooms[ws.ip];
+        if (room) {
+            room.delete(ws.id);
+            if (room.size === 0) {
+                delete rooms[ws.ip];
+            } else {
+                // Notify other peers that this peer has left
+                ws.publish(ws.ip, JSON.stringify({
+                    type: 'peer-left',
+                    peerId: ws.id
+                }));
+            }
+        }
+    },
 });
 
-const server = new SnapdropServer(process.env.PORT || 3000);
+// A simple way to manage state without global variables
+const serverState = {
+    rooms: {},
+};
+app.getRooms = () => serverState.rooms;
+
+// Add a helper to get peer info, attached to the prototype
+uWS.WebSocket.prototype.getInfo = function() {
+    return {
+        id: this.id,
+        name: this.name,
+        rtcSupported: true, // Assume all modern clients support WebRTC
+    };
+};
+
+app.listen(port, (token) => {
+    if (token) {
+        console.log(`Snapdrop is running on port ${port}`);
+    } else {
+        console.log(`Failed to listen to port ${port}`);
+    }
+});
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+    console.info("SIGINT Received, exiting...");
+    process.exit(0);
+});
+
+process.on('SIGTERM', () => {
+    console.info("SIGTERM Received, exiting...");
+    process.exit(0);
+});
